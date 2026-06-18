79de387053a2e7765c260b54d19ec113e2578b3c
juannpz <juannpz@proton.me>
2026-06-17 21:38:54 -0300

    chore: fix ESLint version to v9 for compatibility with eslint-plugin-react

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -2,6 +2,9 @@ import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
 import nextTypescript from "eslint-config-next/typescript";
 
 export default [
+  {
+    ignores: ['next.config.ts', '.next/', '**/.next/**'],
+  },
   ...nextCoreWebVitals,
   ...nextTypescript,
   {
